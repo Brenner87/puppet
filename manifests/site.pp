21e9c6df7762b83ea1b649d7fb77b07079461ac2
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2018-11-01 20:01:57 +0200

    modify site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,12 @@
 node default {
     notify {'This is production environment':}
+    include base
+}
+
+node /^jenkins.*vagrant\.com/ {
+    include 'role::jenkins'
+}
+
+node /^puppetmaster.*vagrant\.com/ {
+    include 'role::puppetmaster'
 }
