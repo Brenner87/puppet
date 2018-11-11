67baecdaabff2b593d77235b21ff4baa4f1c6c9b
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2018-11-11 14:57:11 +0200

    rework jenkins module

---
+++ modules/jenkins/manifests/config.pp
@@ -0,0 +1,32 @@
+class jenkins::config ($jenkins_admin_password){
+
+    $groovy_dir='/var/lib/jenkins/init.groovy.d'
+
+    file {'/etc/sysconfig/jenkins':
+        ensure => 'present',
+        source => 'puppet:///modules/jenkins/jenkins',
+        mode   => '0644',
+        owner  => 'jenkins',
+        group => 'jenkins',
+    }
+
+    file {$groovy_dir:
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    file {"${groovy_dir}/security.groovy":
+        ensure  => 'present',
+        content => template('jenkins/security.groovy.erb'),
+        require  => File[$groovy_dir],
+    }
+    
+    file {"${groovy_dir}/installPlugins.groovy":
+        ensure  => 'present',
+        content => template('jenkins/installPlugins.groovy.erb'),
+        require  => File[$groovy_dir],
+    }
+}
+
---
+++ modules/jenkins/manifests/install.pp
@@ -0,0 +1,15 @@
+class jenkins::install ($base_version,
+                        $jdk_version,
+                        ){
+
+    $packages=['jenkins',]
+    $groovy_dir='/var/lib/jenkins/init.groovy.d'
+
+    package {'java-1.8.0-openjdk.x86_64':
+        ensure => $jdk_version,
+        before => Package['jenkins'],
+    }
+
+    package {'jenkins':
+        ensure => $base_version,
+    }
