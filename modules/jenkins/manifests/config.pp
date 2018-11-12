2a7c70cd21dc07f3130632df67809d697b00a5c2
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2018-11-12 21:07:08 +0200

    fix permissions for jenkins management scripts

--- modules/jenkins/manifests/config.pp
+++ modules/jenkins/manifests/config.pp
@@ -20,12 +20,18 @@ class jenkins::config ($jenkins_admin_password){
     file {"${groovy_dir}/security.groovy":
         ensure  => 'present',
         content => template('jenkins/security.groovy.erb'),
-        require  => File[$groovy_dir],
+        owner   => 'jenkins',
+        group   => 'jenkins',
+        mode    => '0600',
+        require => File[$groovy_dir],
     }
     
     file {"${groovy_dir}/installPlugins.groovy":
         ensure  => 'present',
         content => template('jenkins/installPlugins.groovy.erb'),
+        owner   => 'jenkins',
+        group   => 'jenkins',
+        mode    => '0600',
         require  => File[$groovy_dir],
     }
 }
