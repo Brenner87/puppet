8312af1c5da0e6c68b6c85c94872a004c2ab9530
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2018-11-22 17:28:16 +0200

    changes to puppetmaster class

--- modules/puppetmaster/manifests/init.pp
+++ modules/puppetmaster/manifests/init.pp
@@ -2,4 +2,8 @@ class puppetmaster ($puppet_version){
     package {'puppetserver':
         ensure => $puppet_version,
     }
+
+    service {'puppetserver':
+        ensure => 'running',
+        enable => true,
 }
