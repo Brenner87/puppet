6382ee33262722f0f139035101776895911f20b6
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2018-11-26 15:06:50 +0200

    refactor modules and roles

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -1,9 +1,8 @@
 class jenkins {
-    include docker
     include jenkins::maven
     include jenkins::install
     include jenkins::config
     include jenkins::service
 
-    include Class[docker] -> Class[jenkins::maven] -> Class[jenkins::install] -> Class[jenkins::config] ~> Class[jenkins::service]
+    include Class[jenkins::maven] -> Class[jenkins::install] -> Class[jenkins::config] ~> Class[jenkins::service]
 }
--- modules/role/manifests/jenkins.pp
+++ modules/role/manifests/jenkins.pp
@@ -1,4 +1,7 @@
 class role::jenkins{
     include base
+    include docker
     include jenkins
+
+    include Class['base']->Class['docker']->Class['jenkins']
 }
