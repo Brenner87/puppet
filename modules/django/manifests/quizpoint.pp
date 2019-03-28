65718cee7125072bc792a860c2a152f23baeba60
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-03-28 13:51:02 +0200

    Changes for quizpoint

--- modules/django/manifests/quizpoint.pp
+++ modules/django/manifests/quizpoint.pp
@@ -3,33 +3,8 @@ class django::quizpoint (
     $quizpoint_secret
 )
 {
-    $linux_packages = [
-        'https://centos7.iuscommunity.org/ius-release.rpm',
-        'python36',
-        'python36-pip',
-        'python36-devel',
-    ]
-
-    $pip_packages = [
-        'virtualenv',
-    ]
-
-    yum::group { 'development':
-        ensure  => 'present',
-        timeout => 600,
-    }
-
-    package { $linux_packages:
-        ensure => latest
-    }
-
-#    include pip
-
-    pip::install { $pip_packages:
-        python_version => '3.6',
-        ensure         => present,
-        require        => Package['python36-pip'],
-    }
-
+    django::install_python { "proj_python":
+        python => $quizpoint_params['python']
+        }
        
 }
