49cb2975d1d5e424e5f4aacfa2ceffd88407ddc8
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-03-28 13:53:47 +0200

    Changes for quizpoint

---
+++ modules/django/manifests/install_python.pp
@@ -0,0 +1,25 @@
+define install_python (
+    $python
+){
+    $packages=[$python, "${python}-pip", "${python}-devel"]
+
+    yumrepo {'ius':
+        baseurl     => 'https://dl.iuscommunity.org/pub/ius/stable/CentOS/7',
+        gpgcheck    => 0,
+        description => 'Python repo'
+    }
+
+    yum::group { 'development':
+        ensure  => 'present',
+        timeout => 600,
+        require => Yumrepo['ius'],
+    }
+
+    package {$packages:
+        ensure => present,
+        require => Yumrepo['ius'],
+    }
+
+
+    
+}
