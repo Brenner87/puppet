14989a4297249e6d8e183a3ea5a1676647cdab1c
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-03-28 17:12:40 +0200

    Changes for quizpoint

--- modules/django/manifests/quizpoint.pp
+++ modules/django/manifests/quizpoint.pp
@@ -5,8 +5,17 @@ class django::quizpoint (
 {
     #include python
 
-    python::install_python { "proj_python":
+    python::install_python { 'proj_python':
         python => $quizpoint_params['python']
         }
-       
+
+    python::update_pip { 'update-pip':
+        python => $quizpoint_params['python'],
+        require => Python::Install_python['proj_python']
+    }
+
+    python::install_pip_module {'module':
+        python => $quizpoint_params['python'],
+        require =>Python::Update_pip['update-pip']
+    }   
 }
