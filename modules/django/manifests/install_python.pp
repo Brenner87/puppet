4ddbab38c858e9e838976435bae6d8de1e4bb552
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-03-28 14:48:11 +0200

    Changes for quizpoint

--- modules/django/manifests/install_python.pp
+++ modules/django/manifests/install_python.pp
@@ -3,26 +3,17 @@ define django::install_python (
 ){
     $packages=["ius-release", $python, "${python}-pip", "${python}-devel"]
 
-#    yumrepo {'ius':
-#        baseurl  => 'https://dl.iuscommunity.org/pub/ius/stable/CentOS/7',
-#        gpgcheck => 0,
-#        enabled => true,
-#        descr    => 'Python repo'
-#    }
-
     yum::group { 'development':
         ensure  => 'present',
         timeout => 600,
         require => Package['ius-release'],
     }
 
-    package {$packages:
+    package { $packages:
         ensure => present,
         require => Yumrepo['ius'],
     }
 
     Package['ius-release'] -> Package[$python] -> Package["${python}-devel" -> Pakcage["${python}-pip"]
-
-
-    
+   
 }
