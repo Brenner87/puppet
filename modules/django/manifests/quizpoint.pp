9dd96adde12f77c8cb07bc4977a4465a6dea706a
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-03-29 15:43:06 +0200

    Changes for quizpoint

--- modules/django/manifests/quizpoint.pp
+++ modules/django/manifests/quizpoint.pp
@@ -8,13 +8,47 @@ class django::quizpoint (
     python::install_python { $quizpoint_params['python']: }
 
     python::update_pip { 'update-pip':
-        python => $quizpoint_params['python'],
-        require => Python::Install_python[$quizpoint_params['python']]
+        python    => $quizpoint_params['python'],
+        subscribe => Python::Install_python[$quizpoint_params['python']]
     }
 
     python::install_pip_module {'virtualenv':
-        python => $quizpoint_params['python'],
-        module => 'virtualenv',
+        python  => $quizpoint_params['python'],
+        module  => 'virtualenv',
         require =>Python::Update_pip['update-pip']
-    }   
+    }  
+
+    user {$quizpoint_params['user']:
+        ensure     => present,
+        uid        => 1200,
+        gid        => 1200,
+        managehome => false,
+    }
+
+
+
+    file {$quizpoint_params['proj_path']:
+        ensure  => directory,
+        owner   => $quizpoint_params['user'],
+        group   => 'nginx',
+        require => User[$quizpoint_params['user']],
+    }
+
+    ['config','run_data','logs'].each |$dir| {
+
+        file {"${quizpoint_params['proj_path']}/${dir}":
+            ensure  => directory,
+            owner   => $quizpoint_params['user'],
+            group   => 'nginx',
+            require => File[$quizpoint_params['proj_path']],
+        }
+    }
+
+
+    file {"${quizpoint_params['proj_path']}/logs/${quizpoint_params['proj_name']}.log":
+        ensure  => directory,
+        owner   => $quizpoint_params['user'],
+        group   => 'nginx',
+        require => File["${quizpoint_params['proj_path']}/logs"],
+    }
 }
