a8d47f40339993c7cd774aaf7ed88555c071fd52
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-04-03 22:57:19 +0300

    service changes

--- modules/web/manifests/init.pp
+++ modules/web/manifests/init.pp
@@ -2,4 +2,16 @@ class web {
     package {'nginx':
         ensure => 'latest',
     }
+    
+    file {'':
+        ensure    => 'present',
+        content   => epp('web/nginx.conf.epp'),
+        require   => Package['nginx'],
+        subscribe => Service['nginx']
+    }
+
+    service {'nginx':
+        ensure  => running,
+        enabled => true,
+    }
 }
