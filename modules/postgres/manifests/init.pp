fd405d69dc9f1e088e59c051571cfc6ab8f9845f
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-04-09 21:14:00 +0300

    testing db installation

--- modules/postgres/manifests/init.pp
+++ modules/postgres/manifests/init.pp
@@ -1,16 +1,22 @@
 class postgres {
+    class { 'postgresql::globals':
+        version             => '11',
+        manage_package_repo => true,
+        encoding            => 'UTF8',
+  }->
     class { 'postgresql::server':
         postgres_password => '111',
         #package_name      => 'postgresql11-server'
-    }
-
+    }->
+    postgresql::server::config_entry { 'listen_addresses':
+        value     => $fqdn,
+        #require   => Class['postgresql::server']
+    }->
     postgresql::server::db { 'quizpoint':
         user      => 'quizpoint',
         password  => postgresql_password('quizpoint', '123'),
         #        require   => Class['postgresql::server']
-    }
-
-
+    }->
     postgresql::server::pg_hba_rule { 'allow quizpoint access':
         description        => 'Open access from web server',
         type               => 'host',
@@ -19,8 +25,7 @@ class postgres {
         address            => '192.168.56.113',
         auth_method        => 'md5',
         #require            => Postgresql::Server::Db['quizpoint']
-    }
-
+    }->
     postgresql::server::pg_hba_rule { 'allow quizpoint access from local host':
         description        => 'Open access from local host',
         type               => 'host',
@@ -30,8 +35,4 @@ class postgres {
         auth_method        => 'md5',
         #require            => Postgresql::Server::Db['quizpoint']
     }
-    postgresql::server::config_entry { 'listen_addresses':
-        value     => $fqdn,
-        #require   => Class['postgresql::server']
-}
 }
