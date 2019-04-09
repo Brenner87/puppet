379748582bcc323a8bbcebd70cea9ef065498efd
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-04-09 20:55:23 +0300

    testing db installation

--- modules/postgres/manifests/init.pp
+++ modules/postgres/manifests/init.pp
@@ -7,7 +7,7 @@ class postgres {
     postgresql::server::db { 'quizpoint':
         user      => 'quizpoint',
         password  => postgresql_password('quizpoint', '123'),
-        require   => Class['postgresql::server']
+        #        require   => Class['postgresql::server']
     }
 
 
@@ -18,7 +18,7 @@ class postgres {
         user               => 'quizpoint',
         address            => '192.168.56.113',
         auth_method        => 'md5',
-        require            => Postgresql::Server::Db['quizpoint']
+        #require            => Postgresql::Server::Db['quizpoint']
     }
 
     postgresql::server::pg_hba_rule { 'allow quizpoint access from local host':
@@ -28,10 +28,10 @@ class postgres {
         user               => 'quizpoint',
         address            => '127.0.0.1',
         auth_method        => 'md5',
-        require            => Postgresql::Server::Db['quizpoint']
+        #require            => Postgresql::Server::Db['quizpoint']
     }
     postgresql::server::config_entry { 'listen_addresses':
         value     => $fqdn,
-        require   => Class['postgresql::server']
+        #require   => Class['postgresql::server']
 }
 }
