017722f138f3c2be414fff7d93e3b5297614c8a7
Oleksandr Shalbanov <oshalbanov@themeetgroup.com>
2019-04-09 20:34:58 +0300

    testing db installation

--- modules/postgres/manifests/init.pp
+++ modules/postgres/manifests/init.pp
@@ -1,34 +1,37 @@
 class postgres {
     class { 'postgresql::server':
-        postgres_password          => '111',
+        postgres_password => '111',
+        package_name      => 'postgresql11-server'
     }
 
     postgresql::server::db { 'quizpoint':
-        user     => 'quizpoint',
-        password => postgresql_password('quizpoint', '123'),
+        user      => 'quizpoint',
+        password  => postgresql_password('quizpoint', '123'),
+        subscribe => Class['postgresql::server']
     }
 
 
-        postgresql::server::pg_hba_rule { 'allow quizpoint access':
-    #          description        => 'Open up postgresql for access from 200.1.2.0/24',
-         type               => 'host',
-         database           => 'quizpoint',
-         user               => 'quizpoint',
-         address            => '192.168.56.113',
-         auth_method        => 'md5',
-    #     target             => '/path/to/pg_hba.conf',
-    #     postgresql_version => '9.4',
+    postgresql::server::pg_hba_rule { 'allow quizpoint access':
+        description        => 'Open access from web server',
+        type               => 'host',
+        database           => 'quizpoint',
+        user               => 'quizpoint',
+        address            => '192.168.56.113',
+        auth_method        => 'md5',
+        subscribe          => Postgresql::Server::Db['quizpoint']
     }
 
-        postgresql::server::pg_hba_rule { 'allow quizpoint access from local host':
-    #          description        => 'Open up postgresql for access from 200.1.2.0/24',
-         type               => 'host',
-         database           => 'quizpoint',
-         user               => 'quizpoint',
-         address            => '127.0.0.1',
-         auth_method        => 'md5',
-    #     target             => '/path/to/pg_hba.conf',
-    #     postgresql_version => '9.4',
+    postgresql::server::pg_hba_rule { 'allow quizpoint access from local host':
+        description        => 'Open access from local host',
+        type               => 'host',
+        database           => 'quizpoint',
+        user               => 'quizpoint',
+        address            => '127.0.0.1',
+        auth_method        => 'md5',
+        subscribe          => Postgresql::Server::Db['quizpoint']
     }
-
+    postgresql::server::config_entry { 'listen_addresses':
+        value => $fqdn,
+        subscribe          => Postgresql::Server::Db['quizpoint']
+}
 }
